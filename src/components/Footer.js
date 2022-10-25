1bb1d3052e6bb678e61a42adc671f177e4a1f33b
ronerog <ronerojr@gmail.com>
2022-10-25 15:23:56 -0300

    lint corrigido

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import mealIcon from '../images/mealIcon.svg';
 function Footer() {
   return (
 
-    <footer data-testid="footer">
+    <footer id="footer" data-testid="footer">
       <Link to="/drinks">
         <img src={ drinkIcon } alt="drink" data-testid="drinks-bottom-btn" />
       </Link>
