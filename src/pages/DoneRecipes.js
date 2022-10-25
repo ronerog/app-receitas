bd2144952d2674a73a8e1285330443737aaf408f
Flay000 <flayrys100@gmail.com>
2022-10-25 19:19:32 -0300

    requisito 7, flay

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function DoneRecipes() {
   return (
     <div>
       Receitas Feitas
+      <Header title="Done Recipes" IconSearch={ false } />
     </div>
+
   );
 }
 
