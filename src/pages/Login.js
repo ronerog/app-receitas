a14c75dea99b722d4f136a8e36daad31ae374300
Tárcio Rezende <tarciosrezendd@gmail.com>
2022-10-26 18:53:56 -0300

    pagina login pronta

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import Context from '../context/Context';
 
 function Login() {
+  const { email, password, handleEmail, handlePassword } = useContext(Context);
+  const emailValid = /\S+@\S+\.\S+/.test(email);
+  const minimumPassword = 7;
+  const history = useHistory();
+
+  const saveUser = (event) => {
+    event.preventDefault();
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/meals');
+  };
+
   return (
-    <div>
-      Login
-    </div>
+    <form>
+      <label htmlFor="email">
+        Email
+        <input
+          type="text"
+          name="email"
+          data-testid="email-input"
+          placeholder="Email"
+          value={ email }
+          onChange={ handleEmail }
+        />
+      </label>
+      <label htmlFor="password">
+        Senha
+        <input
+          type="password"
+          name="senha"
+          data-testid="password-input"
+          placeholder="Senha"
+          value={ password }
+          onChange={ handlePassword }
+        />
+      </label>
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        onClick={ saveUser }
+        disabled={ !(password.length >= minimumPassword && emailValid) }
+      >
+        Enter
+      </button>
+    </form>
   );
 }
 
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,25 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('test the login page', () => {
+  test('The login components', () => {
+    renderWithRouter(<App />);
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('password-input');
+    const btn = screen.getByRole('button', { name: /enter/i });
+
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(btn).toHaveAttribute('disabled');
+
+    userEvent.type(email, 'xablau@xablau.com');
+    userEvent.type(password, 'xablau1');
+    userEvent.click(btn);
+    expect(localStorage.setItem).toBeCalled();
+    screen.logTestingPlaygroundURL();
+  });
 });
