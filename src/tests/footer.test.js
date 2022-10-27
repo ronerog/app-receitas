1ceb0b1f454ca5d4b6ad660ddc17ce831b5eafea
ronerog <ronerojr@gmail.com>
2022-10-27 02:42:30 -0300

    lint

--- src/tests/footer.test.js
+++ src/tests/footer.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
-// import renderWithRouter from 'react-router-dom';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Footer from '../components/Footer';
 import { renderWithContext } from './helper/renderWith';
